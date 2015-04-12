7b08c281d1d76f61cecdfa6ab389bf63dddf09d9
Arve Knudsen <arve.knudsen@gmail.com>
2015-04-13 00:50:28 +0200

    insert-project: Add --mongo-url option

--- insert-project.py
+++ insert-project.py
@@ -16,16 +16,20 @@ def _info(msg):
 cl_parser = argparse.ArgumentParser(description='Insert a project into Meteor\'s MongoDB instance')
 cl_parser.add_argument('input', help='JSON input file')
 cl_parser.add_argument('--site', default=None, help='Specify Meteor site (default: localhost)')
+cl_parser.add_argument('--mongo-url', default=None, help='Specify MongoDB URL (default: none)')
 args = cl_parser.parse_args()
 
 with open(args.input) as input_file:
     json = load_json(input_file)
 
-command = ['meteor', 'mongo', '-U']
-if args.site:
-    command.append(args.site)
-_info('Getting Mongo URL...')
-mongo_url = subprocess.check_output(command).decode().strip()
+if not args.mongo_url:
+    command = ['meteor', 'mongo', '-U']
+    if args.site:
+        command.append(args.site)
+    _info('Getting Mongo URL...')
+    mongo_url = subprocess.check_output(command).decode().strip()
+else:
+    mongo_url = args.mongo_url
 db_name = mongo_url.rsplit('/', 1)[1]
 _info('Connecting to MongoDB: {}'.format(mongo_url))
 client = pymongo.MongoClient(mongo_url)
