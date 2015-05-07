db781f5f402059b864593e2968e5ec2da3eacd6b
Arve Knudsen <arve.knudsen@gmail.com>
2015-05-07 18:05:39 -0400

    Introduce staging server

--- docker/create-mongodata.sh
+++ docker/create-mongodata.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 docker create -v /var/mongo:/data/db --name mongodata mongo
+docker create -v /var/mongo.staging:/data/db --name mongodata.staging mongo
--- docker/run-staging.sh
+++ docker/run-staging.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-docker run --name mongo.staging -d -p 27017:27017 --volumes-from mongodata --restart always mongo:3.0.2 --auth
+docker run --name mongo.staging -d -p 27018:27017 --volumes-from mongodata.staging --restart always mongo:3.0.2 --auth
 docker run --name muzhack.staging -d --restart always --env-file staging.env --link mongo.staging:db aknudsen/muzhack
-docker run -d --restart always --name nginx.staging -v /etc/nginx/ssl/bundle.crt:/bundle.crt -v /etc/nginx/ssl/private.key:/private.key --link muzhack.staging:backend --publish 443:443 meteorhacks/mup-frontend-server /start.sh
