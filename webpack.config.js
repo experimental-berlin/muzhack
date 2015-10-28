8a5708f7897054819bfab349cad6829271aed259
Arve Knudsen <arve.knudsen@gmail.com>
2015-10-28 16:50:47 +0100

    Fix font loading

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         loader: 'style-loader!css-loader',
       },
       {
-        test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg)($|\?)/,
         loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]',
       },
     ],
