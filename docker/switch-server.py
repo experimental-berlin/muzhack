b63ad863a307981e6707d3e8f9712e2cc87bfad1
Arve Knudsen <arve.knudsen@gmail.com>
2015-10-31 20:08:10 +0100

    Extend production switching script

--- docker/switch-server.py
+++ docker/switch-server.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python3
 import argparse
 import subprocess
+import json
+import sys
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('server', choices=['blue', 'green',],
-    help='Specify server to switch to')
 args = parser.parse_args()
 
-server = 'muzhack-{}'.format(args.server)
 
-subprocess.check_call(['tutum', 'service', 'set', '--link', '{0}:{0}'.format(server), 'lb'],
-    stdout=subprocess.PIPE)
-print('* Successfully switched production server to {}'.format(server))
+def info(msg):
+    sys.stdout.write('* {}\n'.format(msg))
+    sys.stdout.flush()
+
+info('Determining current production details...')
+output = subprocess.check_output(['tutum', 'service', 'inspect', 'lb.muzhack-staging']).decode(
+    'utf-8')
+data = json.loads(output)
+linked_service = data['linked_to_service'][0]['name']
+info('Currently linked service is \'{}\''.format(linked_service))
+
+if linked_service == 'muzhack-green':
+    link_to = 'muzhack-blue'
+else:
+    assert linked_service == 'muzhack-blue'
+    link_to = 'muzhack-green'
+
+info('Redeploying service \'{}\'...'.format(link_to))
+subprocess.check_call(['tutum', 'service', 'redeploy', '--sync', link_to], stdout=subprocess.PIPE)
+
+info('Linking to service \'{}\'...'.format(link_to))
+subprocess.check_call(['tutum', 'service', 'set', '--link-service', '{0}:{0}'.format(link_to),
+    '--sync', 'lb'], stdout=subprocess.PIPE)
+info('Successfully switched production service to {}'.format(link_to))
