1d3f931b17c27df072bf0bf45b2299e14d9c756a
Arve Knudsen <arve.knudsen@gmail.com>
2015-11-02 11:36:54 +0100

    Handle Tutum errors

--- docker/update-production.py
+++ docker/update-production.py
@@ -9,16 +9,25 @@ parser = argparse.ArgumentParser()
 args = parser.parse_args()
 
 
-def info(msg):
+def _info(msg):
     sys.stdout.write('* {}\n'.format(msg))
     sys.stdout.flush()
 
-info('Determining current production details...')
+
+def _run_tutum(args):
+    try:
+        subprocess.check_call(['tutum',] + args, stdout=subprocess.PIPE)
+    except subprocess.CalledProcessError as err:
+        sys.stderr.write('{}\n'.format(err))
+        sys.exit(1)
+
+
+_info('Determining current production details...')
 output = subprocess.check_output(['tutum', 'service', 'inspect', 'lb.muzhack-staging']).decode(
     'utf-8')
 data = json.loads(output)
 linked_service = data['linked_to_service'][0]['name']
-info('Currently linked service is \'{}\''.format(linked_service))
+_info('Currently linked service is \'{}\''.format(linked_service))
 
 if linked_service == 'muzhack-green':
     link_to = 'muzhack-blue'
@@ -26,10 +35,11 @@ else:
     assert linked_service == 'muzhack-blue'
     link_to = 'muzhack-green'
 
-info('Redeploying service \'{}\'...'.format(link_to))
+_info('Redeploying service \'{}\'...'.format(link_to))
+_run_tutum(['service', 'redeploy', '--sync', link_to,])
 subprocess.check_call(['tutum', 'service', 'redeploy', '--sync', link_to], stdout=subprocess.PIPE)
 
-info('Linking to service \'{}\'...'.format(link_to))
-subprocess.check_call(['tutum', 'service', 'set', '--link-service', '{0}:{0}'.format(link_to),
-    '--sync', 'lb.muzhack-staging'], stdout=subprocess.PIPE)
-info('Successfully switched production service to {}'.format(link_to))
+_info('Linking to service \'{}\'...'.format(link_to))
+_run_tutum(['service', 'set', '--link-service', '{0}:{0}'.format(link_to),
+    '--sync', 'lb.muzhack-staging',])
+_info('Successfully switched production service to {}'.format(link_to))
