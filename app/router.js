8079bac4010002e4aae15f7993f7b0cca61d85c9
Arve Knudsen <arve.knudsen@gmail.com>
2015-11-14 22:50:59 +0100

    More robust routing

--- app/router.js
+++ app/router.js
@@ -19,6 +19,9 @@ let normalizePath = (path) => {
   if (path[0] !== '/') {
     path = `/${path}`
   }
+  while (path.endsWith('/') && path.length > 1) {
+    path = path.slice(0, -1)
+  }
   return path
 }
 
