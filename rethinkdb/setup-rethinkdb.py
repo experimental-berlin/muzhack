32301fd4600f061bd3db4a1d564a416fc1d4d670
Arve Knudsen <arve.knudsen@gmail.com>
2015-11-15 11:12:06 -0500

    Also set auth_key on RethinkDB

--- rethinkdb/setup-rethinkdb.py
+++ rethinkdb/setup-rethinkdb.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 import rethinkdb as r
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Set up RethinkDB locally')
+parser.add_argument('auth_key', help='Specify RethinkDB authentication key')
+args = parser.parse_args()
 
 conn = r.connect()
+r.db('rethinkdb').table('cluster_config').get('auth').update({'auth_key': args.auth_key,}).run(conn)
 r.db_create('muzhack').run(conn)
 r.db('muzhack').table_create('users').run(conn)
 r.db('muzhack').table_create('projects').run(conn)
