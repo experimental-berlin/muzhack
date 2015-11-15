6ab7937ec7843e09394192dead66a5a9a048b116
Arve Knudsen <arve.knudsen@gmail.com>
2015-11-15 18:24:22 +0100

    Fix avatar

--- app/views/userProfile/vcard.js
+++ app/views/userProfile/vcard.js
@@ -3,18 +3,34 @@ let component = require('omniscient')
 let h = require('react-hyperscript')
 let logger = require('js-logger-aknudsen').get('userProfile')
 let S = require('underscore.string.fp')
+let CryptoJS = require('crypto-js')
 
 let account = require('../../account')
 let {nbsp,} = require('../../specialChars')
 let datetime = require('../../datetime')
 
+let getAvatarUrl = (user) => {
+  if (!S.isBlank(user.avatarUrl)) {
+    logger.debug(`User has an avatar URL: '${user.avatarUrl}'`)
+    return user.avatarUrl
+  } else {
+    logger.debug(`User has no avatar URL, using Gravatar`)
+    let email = user.email
+    let hash = CryptoJS.MD5(email).toString(CryptoJS.enc.Hex)
+    let gravatarUrl = `http://www.gravatar.com/avatar/${hash}?d=identicon&s=230`
+    logger.debug(`Generated gravatar url:`, gravatarUrl)
+    return gravatarUrl
+  }
+}
+
 module.exports = component('VCard', (user) => {
   let userProfileUrl = account.getUserProfileUrl(user.username)
   let userJoined = datetime.displayDateTextual(user.createdAt)
+
   return h('#vcard', [
     h('#user-avatar', [
       h('a', {href: userProfileUrl,}, [
-        h('img', {src: user.avatar, width: 230, height: 230,}),
+        h('img', {src: getAvatarUrl(user), width: 230, height: 230,}),
       ]),
     ]),
     h('#user-name', user.name),
