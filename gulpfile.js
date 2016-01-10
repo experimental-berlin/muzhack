a52c0752e04ad1e447c4cc2bed75e6d41163583c
Arve Knudsen <arve.knudsen@gmail.com>
2016-01-10 14:23:36 +0100

    Fix building of .js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,25 @@
 'use strict'
 let gulp = require('gulp')
 var babel = require('gulp-babel')
+let runSequence = require('run-sequence')
 
-gulp.task('default', () => {
-  return gulp.src('app/**/*.js')
+gulp.task('appCode', () => {
+  return gulp
+    .src('app/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('dist/app'))
+})
+
+gulp.task('libCode', () => {
+  return gulp
+    .src('lib/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dist/lib'))
+})
+
+gulp.task('default', () => {
+  return runSequence(
+    'libCode',
+    'appCode'
+  )
 })
