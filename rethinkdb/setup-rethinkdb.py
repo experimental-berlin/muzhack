f253feac7a4c53bd17958b0c74adbec528ae2e17
Arve Knudsen <arve.knudsen@gmail.com>
2016-01-11 10:58:30 +0100

    Allow setting up rethinkdb remotely

--- rethinkdb/setup-rethinkdb.py
+++ rethinkdb/setup-rethinkdb.py
@@ -3,10 +3,11 @@ import rethinkdb as r
 import argparse
 
 
-parser = argparse.ArgumentParser(description='Set up RethinkDB locally')
+parser = argparse.ArgumentParser(description='Set up RethinkDB')
+parser.add_argument('-H', '--host', default='localhost', help='Specify host')
 args = parser.parse_args()
 
-conn = r.connect()
+conn = r.connect(host=args.host)
 r.db_create('muzhack').run(conn)
 r.db('muzhack').table_create('users').run(conn)
 r.db('muzhack').table_create('projects').run(conn)
