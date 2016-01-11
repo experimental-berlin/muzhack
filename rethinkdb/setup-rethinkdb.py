fa560cf3cd18e3682dd54e2c020ad7f647e4f34d
Arve Knudsen <arve.knudsen@gmail.com>
2016-01-11 11:48:51 +0100

    Allow for auth key

--- rethinkdb/setup-rethinkdb.py
+++ rethinkdb/setup-rethinkdb.py
@@ -5,9 +5,13 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Set up RethinkDB')
 parser.add_argument('-H', '--host', default='localhost', help='Specify host')
+parser.add_argument('-a', '--auth-key', help='Specify authentication key')
 args = parser.parse_args()
 
-conn = r.connect(host=args.host)
+kwargs = {'host': args.host}
+if args.auth_key:
+    kwargs['auth_key'] = args.auth_key
+conn = r.connect(**kwargs)
 r.db_create('muzhack').run(conn)
 r.db('muzhack').table_create('users').run(conn)
 r.db('muzhack').table_create('projects').run(conn)
