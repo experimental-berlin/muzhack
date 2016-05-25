20aa8b2355821be853b029de64fccd1e256b4f02
Arve Knudsen <arve.knudsen@gmail.com>
2016-05-25 14:34:07 +0200

    Fix input validation

--- app/validationFunctions.js
+++ app/validationFunctions.js
@@ -1,16 +1,27 @@
 'use strict'
+let logger = require('js-logger-aknudsen').get('validationFunctions')
 let R = require('ramda')
+let S = require('underscore.string.fp')
 
 module.exports = {
-  isEmpty: R.isEmpty,
-  isEmptyOrHasSpace: (input) => {
-    let isEmpty = R.isEmpty(input)
+  isBlank: S.isBlank,
+  isBlankOrHasSpace: (input) => {
+    let isBlank = S.isBlank(input)
+    if (isBlank) {
+      logger.debug(`Input is blank: '${input}'`)
+    }
     let hasSpace = input.indexOf(' ') > -1
-    return (isEmpty || hasSpace)
+    if (hasSpace) {
+      logger.debug(`Input has space: '${input}'`)
+    }
+    return (isBlank || hasSpace)
   },
   hasSpecialChars: (input) => {
     let val = R.toLower(input)
-    return R.match(/[a-z_]/g, val).length !== val.length
+    logger.debug(`Checking '${input}' for special chars`)
+    let match = R.match(/[a-z_]/gi, input)
+    logger.debug(`Match:`, match)
+    return match.length !== input.length
   },
   areNotTheSame: (inputA, inputB) => {
     return inputA !== inputB
