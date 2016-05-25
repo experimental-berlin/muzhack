a62af16f78d78718a51b41b41eeac6fe16911a21
Arve Knudsen <arve.knudsen@gmail.com>
2016-05-25 15:08:06 +0200

    Remove debug code

--- app/validationFunctions.js
+++ app/validationFunctions.js
@@ -7,20 +7,12 @@ module.exports = {
   isBlank: S.isBlank,
   isBlankOrHasSpace: (input) => {
     let isBlank = S.isBlank(input)
-    if (isBlank) {
-      logger.debug(`Input is blank: '${input}'`)
-    }
     let hasSpace = input.indexOf(' ') > -1
-    if (hasSpace) {
-      logger.debug(`Input has space: '${input}'`)
-    }
     return (isBlank || hasSpace)
   },
   hasSpecialChars: (input) => {
     let val = R.toLower(input)
-    logger.debug(`Checking '${input}' for special chars`)
     let match = R.match(/[a-z_]/gi, input)
-    logger.debug(`Match:`, match)
     return match.length !== input.length
   },
   areNotTheSame: (inputA, inputB) => {
