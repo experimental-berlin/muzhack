cf78864f3d02dc3244f82fc39672185fa59db58d
Arve Knudsen <arve.knudsen@gmail.com>
2016-06-07 16:07:35 +0200

    Update files

--- docker/generate-specs.py
+++ docker/generate-specs.py
@@ -30,7 +30,7 @@ data = {
         'numReplicas': 2,
         'imageVariant': '',
         'imageTag': 'v1.1.32',
-        'imageProcessorImageTag': 'v1.1.14',
+        'imageProcessorImageTag': 'v1.1.16',
         'imagePullPolicy': 'IfNotPresent',
         'appUri': 'https://muzhack.com',
         's3Bucket': 'muzhack.com',
