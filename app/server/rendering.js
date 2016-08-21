ab34f03bb52f3f6a3dbd56bc217577bdb9b1dd27
Arve Knudsen <arve.knudsen@gmail.com>
2016-08-22 00:08:11 +0200

    Tweak description

--- app/server/rendering.js
+++ app/server/rendering.js
@@ -59,7 +59,7 @@ let renderIndex = (request, reply) => {
     metaHtmlAttributes: {
       name: {
         viewport: 'width=device-width, initial-scale=1.0',
-        description: 'Freely share your music hardware projects.',
+        description: 'An online hub for the free sharing of music hardware projects.',
       },
       property: {
         'fb:app_id': fbAppId,
