3ae5f51c94677802f110bf4ab299840b8f86af2e
Arve Knudsen <arve.knudsen@gmail.com>
2016-08-21 15:04:37 +0200

    Bump version

--- docker/generate-specs.py
+++ docker/generate-specs.py
@@ -30,7 +30,7 @@ data = {
         'appEnvironment': 'production',
         'numReplicas': 2,
         'imageVariant': '',
-        'imageTag': 'v1.3.1',
+        'imageTag': 'v1.3.2',
         'projectProcessorImageTag': 'v1.2.0',
         'imagePullPolicy': 'IfNotPresent',
         'appUri': 'https://muzhack.com',
