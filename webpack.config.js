421a00d80ea84dce2e75a2ac0dd1fd5d208df4ad
Arve Knudsen <arve.knudsen@gmail.com>
2016-08-24 08:57:00 +0200

    Unconditionally enable production React for now

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,17 @@
 'use strict'
 let webpack = require('webpack')
-let isProduction = process.env.NODE_ENV === 'production'
 
 let plugins = [
   new webpack.DefinePlugin({
     __IS_BROWSER__: true,
   }),
 ]
-if (isProduction) {
-   // Disable development features in React in production build
-  plugins = plugins.concat(new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify('production'),
-    },
-  }))
-}
+ // Disable development features in React in production build
+plugins = plugins.concat([new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production'),
+  },
+}),])
 
 module.exports = {
   context: __dirname + '/app',
