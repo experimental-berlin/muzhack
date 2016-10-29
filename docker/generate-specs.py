4ab22048d4f7007992335329c72cac4e29102217
Arve Knudsen <arve.knudsen@gmail.com>
2016-10-29 18:32:41 +0200

    Improve generate-specs

--- docker/generate-specs.py
+++ docker/generate-specs.py
@@ -19,83 +19,88 @@ def _render_template(fname, environment, context):
     merged_context = {**context[environment], **{'environment': environment, }}
     output = template.render(**merged_context)
 
+    dpath = os.path.join('docker/', environment)
     if not os.path.exists(os.path.join(dpath, os.path.dirname(fname))):
         os.makedirs(os.path.join(dpath, os.path.dirname(fname)))
     with open(os.path.join(dpath, fname + '.yaml'), 'wt') as f:
         f.write(output + '\n')
 
-fb_app_id = '274051199640220'
-data = {
-    'production': {
-        'appEnvironment': 'production',
-        'numReplicas': 2,
-        'imageVariant': '',
-        'imageTag': 'v1.3.24',
-        'projectProcessorImageTag': 'v1.2.1',
-        'imagePullPolicy': 'IfNotPresent',
-        'appUri': 'https://muzhack.com',
-        's3Bucket': 'muzhack.com',
-        'fbAppId': fb_app_id,
-        'rethinkdbHost': 'rethinkdb-proxy',
-        'rethinkdbClusterHost1': 'rethinkdb1',
-        'rethinkdbClusterHost2': 'rethinkdb2',
-        'discourseUrl': 'http://forums.muzhack.com',
-        'gcloudBucket': 'muzhack.com',
-        'gcloudProjectId': 'muzhack-1288',
-        'gcloudClientEmail': 'muzhack@muzhack-1288.iam.gserviceaccount.com'
-    },
-    'staging': {
-        'appEnvironment': 'staging',
-        'numReplicas': 1,
-        'imageVariant': '-test',
-        'imageTag': 'latest',
-        'projectProcessorImageTag': 'latest',
-        'imagePullPolicy': 'Always',
-        'appUri': 'https://staging.muzhack.com',
-        's3Bucket': 'staging.muzhack.com',
-        'fbAppId': fb_app_id,
-        'rethinkdbHost': 'rethinkdb-driver',
-        'discourseUrl': 'http://forums.muzhack.com',
-        'gcloudBucket': 'staging.muzhack.com',
-        'gcloudProjectId': 'muzhack-1288',
-        'gcloudClientEmail': 'muzhack@muzhack-1288.iam.gserviceaccount.com'
-    },
-}
 
-with open(os.path.join('docker', 'secrets.json'), 'rt') as f:
-    secrets_raw = json.load(f)
-
-secrets = {}
-for environment, values_raw in secrets_raw.items():
-    secrets[environment] = {
-        k: base64.b64encode(bytes(v, 'utf-8')).decode() for
-        k, v in values_raw.items()
+def _main():
+    fb_app_id = '274051199640220'
+    data = {
+        'production': {
+            'appEnvironment': 'production',
+            'numReplicas': 2,
+            'imageVariant': '',
+            'imageTag': 'v1.3.24',
+            'projectProcessorImageTag': 'v1.2.1',
+            'imagePullPolicy': 'IfNotPresent',
+            'appUri': 'https://muzhack.com',
+            's3Bucket': 'muzhack.com',
+            'fbAppId': fb_app_id,
+            'rethinkdbHost': 'rethinkdb-proxy',
+            'rethinkdbClusterHost1': 'rethinkdb1',
+            'rethinkdbClusterHost2': 'rethinkdb2',
+            'discourseUrl': 'http://forums.muzhack.com',
+            'gcloudBucket': 'muzhack.com',
+            'gcloudProjectId': 'muzhack-1288',
+            'gcloudClientEmail': 'muzhack@muzhack-1288.iam.gserviceaccount.com'
+        },
+        'staging': {
+            'appEnvironment': 'staging',
+            'numReplicas': 1,
+            'imageVariant': '-test',
+            'imageTag': 'latest',
+            'projectProcessorImageTag': 'latest',
+            'imagePullPolicy': 'Always',
+            'appUri': 'https://staging.muzhack.com',
+            's3Bucket': 'staging.muzhack.com',
+            'fbAppId': fb_app_id,
+            'rethinkdbHost': 'rethinkdb-driver',
+            'discourseUrl': 'http://forums.muzhack.com',
+            'gcloudBucket': 'staging.muzhack.com',
+            'gcloudProjectId': 'muzhack-1288',
+            'gcloudClientEmail': 'muzhack@muzhack-1288.iam.gserviceaccount.com'
+        },
     }
 
-for environment in ['staging', 'production', ]:
-    dpath = os.path.join('docker', environment)
-    if not os.path.exists(dpath):
-        os.makedirs(dpath)
+    with open(os.path.join('docker', 'secrets.json'), 'rt') as f:
+        secrets_raw = json.load(f)
+
+    secrets = {}
+    for environment, values_raw in secrets_raw.items():
+        secrets[environment] = {
+            k: base64.b64encode(bytes(v, 'utf-8')).decode() for
+            k, v in values_raw.items()
+        }
+
+    for environment in ['staging', 'production', ]:
+        dpath = os.path.join('docker', environment)
+        if not os.path.exists(dpath):
+            os.makedirs(dpath)
+
+        for fname in [
+            'l7-ingress',
+            'web-controller',
+            'web-service',
+            'rethinkdb/staging',
+        ]:
+            _render_template(fname, environment, data)
+
+        for fname in [
+            'env-secret',
+            'web-secret',
+        ]:
+            _render_template(fname, environment, secrets)
 
     for fname in [
-        'l7-ingress',
-        'web-controller',
-        'web-service',
-        'rethinkdb/staging',
+        'rethinkdb/proxy-deployment',
+        'rethinkdb/proxy-service',
+        'rethinkdb/backup',
     ]:
-        _render_template(fname, environment, data)
+        _render_template(fname, 'production', data)
 
-    for fname in [
-        'env-secret',
-        'web-secret',
-    ]:
-        _render_template(fname, environment, secrets)
+    _render_template('backup-secret', 'production', secrets)
 
-for fname in [
-    'rethinkdb/proxy-deployment',
-    'rethinkdb/proxy-service',
-    'rethinkdb/backup',
-]:
-    _render_template(fname, 'production', data)
-
-_render_template('backup-secret', 'production', secrets)
+_main()
